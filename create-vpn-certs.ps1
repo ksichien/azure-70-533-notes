2c1833e06c887c0524c3790da670460485add553
K Sichien <ksichien@posteo.net>
2017-11-16 13:56:39 +0100

    added script

---
+++ create-vpn-certs.ps1
@@ -0,0 +1,13 @@
+# The root certificate's public key needs to be exported to Azure in a base-64 encoded X.509 format.
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign `
+-KeyUsage CertSign
+
+# The child certificate will be used by the computer running the VPN client.
+New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -Signer $cert `
+-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
